680610035e5d3818226ca7fd3cf8f33ef25228fb
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-05-28 23:44:52 +0330

    create adminCategory

--- src/pages/admin/adminCategory/index.tsx
+++ src/pages/admin/adminCategory/index.tsx
@@ -14,20 +14,25 @@ const adminCategory = () => {
             placeholder="دسته بندی را وارد کنید"
             className="p-2 rounded-xl w-4/6"
           />
-          <button className=" text-center bg-meGreen text-meWhite rounded-xl p-2 w-4/6">
+          <button className=" text-center bg-meGreen hover:bg-meWhite hover:text-meGreen hover:border-2 hover:border-meGreen text-meWhite rounded-xl p-2 w-4/6">
             اضافه کردن
           </button>
         </form>
         <form
           action=""
-          className=" flex flex-col gap-4 items-center border-b-2 border-meBlack2 pb-4"
+          className=" flex flex-col gap-4 items-center border-b-2 border-meBlack2 pb-4 mt-4"
         >
           <input
             type="text"
-            placeholder="دسته بندی را وارد کنید"
+            placeholder="زیر دسته را وارد کنید"
             className="p-2 rounded-xl w-4/6"
           />
-          <button className=" text-center bg-meGreen text-meWhite rounded-xl p-2 w-4/6">
+          <select name="" id="" className="p-2 rounded-xl w-4/6">
+            <option value="" selected hidden>
+              دسته بندی
+            </option>
+          </select>
+          <button className=" text-center bg-meGreen hover:bg-meWhite hover:text-meGreen hover:border-2 hover:border-meGreen text-meWhite rounded-xl p-2 w-4/6">
             اضافه کردن
           </button>
         </form>
