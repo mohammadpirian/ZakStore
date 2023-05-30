2f5e87e1f24959d2930ee254890309c09d52f6f7
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-05-31 00:27:58 +0330

    i have problem for creat subCategory

--- src/pages/admin/adminCategory/index.tsx
+++ src/pages/admin/adminCategory/index.tsx
@@ -1,11 +1,19 @@
 import { AdminLayout } from "@/layout";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 
 const createCategory = async (categoryName) => {
   const response = await axios.post("http://localhost:8000/api/categories", {
     name: categoryName,
+  });
+  return response.data;
+};
+// ==================
+const createSubCategory = async (selectCategory, subCategoryName) => {
+  const response = await axios.post("http://localhost:8000/api/subcategories", {
+    category: selectCategory,
+    name: subCategoryName,
   });
   return response.data;
 };
@@ -16,7 +24,9 @@ const fetchData = async (url: string) => {
 };
 
 const AdminCategory = () => {
+  const [selectCategory, setSelectCategory] = useState("");
   const mutation = useMutation(createCategory);
+  const mutationsub = useMutation(createSubCategory);
 
   const {
     data: data1,
@@ -32,6 +42,17 @@ const AdminCategory = () => {
     const categoryName = event.target.elements.categoryName.value;
     mutation.mutate(categoryName);
   };
+
+  const handleSubmitSub = (event) => {
+    event.preventDefault();
+    const subCategoryName = event.target.elements.subCategoryName.value;
+    console.log(subCategoryName, selectCategory);
+    mutationsub.mutate(selectCategory, subCategoryName);
+  };
+
+  if (isLoading1) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div dir="rtl" className="bg-meWhite p-4 mt-4 rounded-3xl flex">
@@ -52,18 +73,34 @@ const AdminCategory = () => {
           </button>
         </form>
         <form
+          onSubmit={handleSubmitSub}
           action=""
           className=" flex flex-col gap-4 items-center border-b-2 border-meBlack2 pb-4 mt-4"
         >
           <input
             type="text"
+            name="subCategoryName"
             placeholder="زیر دسته را وارد کنید"
             className="p-2 rounded-xl w-4/6"
           />
-          <select name="" id="" className="p-2 rounded-xl w-4/6">
+          <select
+            onChange={(e) => {
+              setSelectCategory(e.target.value);
+            }}
+            name="selectCategory"
+            id=""
+            className="p-2 rounded-xl w-4/6"
+          >
             <option value="" selected hidden>
               دسته بندی
             </option>
+            {data1?.categories.map((item) => {
+              return (
+                <option key={item._id} value={item._id}>
+                  {item.name}
+                </option>
+              );
+            })}
           </select>
           <button className=" text-center bg-meGreen hover:bg-meWhite hover:text-meGreen hover:border-2 hover:border-meGreen text-meWhite rounded-xl p-2 w-4/6">
             اضافه کردن
