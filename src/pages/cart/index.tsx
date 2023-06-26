349c296639d7a2784e397070c31d27ca768da5a3
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-06-26 20:35:53 +0330

    add useEffect for count total price of cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,9 @@
 import CardCart from "@/components/Card/cardCart";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { handeleAddTOCart, handeleEmptyCart } from "@/Redux/slices/cartSlices";
 import { useDispatch, useSelector } from "react-redux";
+
+
 
 const Cart = () => {
   const [totalPriceCart, setTotalPriceCart] = useState(0);
@@ -9,12 +11,18 @@ const Cart = () => {
   console.log(cart);
   const dispatch = useDispatch();
 
+useEffect(() => {
+  const mamad = cart.reduce((sum,item)=>sum+item.totalPriceproduct,0)
+    setTotalPriceCart(mamad);
+  
+});
+
   return (
     <div className="pt-16 bg-meMain flex" dir="rtl">
-      <div className="w-9/12 p-4">
+      <div className="w-9/12 p-4 flex flex-col gap-4">
         {cart.map((item) => {
-          // setTotalPriceCart(totalPriceCart + item.totalPriceproduct);
-          return <CardCart item={item} />;
+          // setTotalPriceCart((prev)=>prev + item.totalPriceproduct);
+          return <CardCart key={item._id} item={item} />;
         })}
       </div>
       <div className="w-3/12 p-4 ">
@@ -22,7 +30,7 @@ const Cart = () => {
           <div className="flex justify-between items-center w-full">
             <p className="text-sm text-meHalfBlack">مجموع خرید شما:</p>
             <p className="text-sm text-meHalfBlack">
-              تومان <span>2500000</span>
+              تومان <span>{totalPriceCart}</span>
             </p>
           </div>
           <button className="w-11/12 text-sm bg-red-600 text-white p-3 rounded-md">
