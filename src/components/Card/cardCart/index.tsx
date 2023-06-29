a9701d027b429976dcc2292e5c56c1d946d2c640
mohammadpiriyan <50291430+mohammadpiriyan@users.noreply.github.com>
2023-06-30 02:37:39 +0330

    add deleteproduct cart modal

--- src/components/Card/cardCart/index.tsx
+++ src/components/Card/cardCart/index.tsx
@@ -1,26 +1,28 @@
-import React from "react";
-import {
-  handeleAddTOCart,
-  handeleEmptyCart,
-  handeleRemoveFromCart,
-} from "@/Redux/slices/cartSlices";
+import React, { useState } from "react";
+
 import { useDispatch } from "react-redux";
 import Link from "next/link";
+import DeleteProductCartModal from "@/components/Modal/DeleteProductCartModal";
 
 const CardCart = ({ item }) => {
+  const [isOpenDeleteProductCartModal, setIsOpenDeleteProductCartModal] =
+    useState(false);
   console.log(item);
   const dispatch = useDispatch();
   return (
     <div className="bg-white p-4 flex rounded-xl justify-between">
       <div className="flex">
         <div className="w-40 p-4 border-2 rounded-xl">
-        <Link
-      href={{
-        pathname: `/SingleProduct/${item.name}`,
-        query: { id: item._id },
-      }}
-    >
-          <img src={`${process.env.BASE_IMAGE_URL}${item?.images[0]}`} alt="" />
+          <Link
+            href={{
+              pathname: `/SingleProduct/${item.name}`,
+              query: { id: item._id },
+            }}
+          >
+            <img
+              src={`${process.env.BASE_IMAGE_URL}${item?.images[0]}`}
+              alt=""
+            />
           </Link>
         </div>
         <div className="py-2 px-4 flex flex-col justify-between">
@@ -30,26 +32,46 @@ const CardCart = ({ item }) => {
               <p className="text-sm text-meBlueText">
                 {item?.category?.name} | {item?.subcategory?.name}
               </p>
-              
-            <div className="flex gap-1"><img className='w-4' src="/images/icon/achievement.png" alt="" /><p className="text-xs text-meHalfBlack">{item?.brand}</p></div>
+
+              <div className="flex gap-1">
+                <img
+                  className="w-4"
+                  src="/images/icon/achievement.png"
+                  alt=""
+                />
+                <p className="text-xs text-meHalfBlack">{item?.brand}</p>
+              </div>
             </div>
           </div>
           <div className="flex flex-col gap-[0.1rem]">
-            <div className="flex gap-1"><img className='w-4' src="/images/icon/warranty.png" alt="" /><p className="text-xs ">
-              گارانتی اصالت کالا {item?.brand}
-            </p> </div>
-            <div className="flex gap-1"><img className='w-4' src="/images/icon/bag.png" alt="" /><p className="text-xs text-meHalfBlack">ارسال زاک استور </p></div>
-            <div className="flex gap-1"><img className='w-4' src="/images/icon/delivery.png" alt="" /><p className="text-xs text-meHalfBlack">ارسال فوری (شهر تهران)</p> </div>
-            <div className="flex gap-1"><img className='w-4' src="/images/icon/price.png" alt="" /><p className="text-xs text-meHalfBlack">{item?.price} تومان</p> </div>
+            <div className="flex gap-1">
+              <img className="w-4" src="/images/icon/warranty.png" alt="" />
+              <p className="text-xs ">گارانتی اصالت کالا {item?.brand}</p>{" "}
+            </div>
+            <div className="flex gap-1">
+              <img className="w-4" src="/images/icon/bag.png" alt="" />
+              <p className="text-xs text-meHalfBlack">ارسال زاک استور </p>
+            </div>
+            <div className="flex gap-1">
+              <img className="w-4" src="/images/icon/delivery.png" alt="" />
+              <p className="text-xs text-meHalfBlack">
+                ارسال فوری (شهر تهران)
+              </p>{" "}
+            </div>
+            <div className="flex gap-1">
+              <img className="w-4" src="/images/icon/price.png" alt="" />
+              <p className="text-xs text-meHalfBlack">
+                {item?.price} تومان
+              </p>{" "}
+            </div>
           </div>
-          
         </div>
       </div>
 
       <div className="flex flex-col justify-between items-end">
         <button
           className=" w-6 h-6"
-          onClick={() => dispatch(handeleRemoveFromCart(item))}
+          onClick={() => setIsOpenDeleteProductCartModal(item)}
         >
           <img src="/images/icon/close3.png" alt="" />
         </button>
@@ -62,6 +84,12 @@ const CardCart = ({ item }) => {
           </p>
         </div>
       </div>
+      {isOpenDeleteProductCartModal && (
+        <DeleteProductCartModal
+          isOpenDeleteProductCartModal={isOpenDeleteProductCartModal}
+          setIsOpenDeleteProductCartModal={setIsOpenDeleteProductCartModal}
+        />
+      )}
     </div>
   );
 };
---
+++ src/components/Modal/DeleteProductCartModal/index.tsx
@@ -0,0 +1,100 @@
+import { request } from "@/utils/request";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import { toast } from "react-toastify";
+import Cookies from "universal-cookie";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
+import {
+    handeleAddTOCart,
+    handeleEmptyCart,
+    handeleRemoveFromCart,
+  } from "@/Redux/slices/cartSlices";
+
+export default function DeleteProductCartModal({
+    isOpenDeleteProductCartModal, setIsOpenDeleteProductCartModal
+}) {
+  const cookie = new Cookies();
+  const isModalOpen = isOpenDeleteProductCartModal ? true : false;
+  const dispatch = useDispatch();
+
+  function closeModal() {
+    setIsOpenDeleteProductCartModal(false);
+  }
+
+  const handleDelete = (isOpenDeleteProductCartModal) => {
+    dispatch(handeleRemoveFromCart(isOpenDeleteProductCartModal))
+    toast.info("حذف محصول با موفقیت انجام شد!");
+    setIsOpenDeleteProductCartModal(false);
+  };
+
+  return (
+    <>
+      <Transition appear show={isModalOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          dir="rtl"
+          className="relative z-10"
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900 flex"
+                  >
+                    حذف کالا
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500 flex">
+                      آیا مطمئن هستید از حذف این کالا؟
+                    </p>
+                  </div>
+
+                  <div className="mt-4 flex gap-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      بیخیال
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                      onClick={() => handleDelete(isOpenDeleteProductCartModal)}
+                    >
+                      حذف
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
